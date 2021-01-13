1f1756d7023d9ab89beb88449b6c893b61313060
杨贵添 <1027275314@qq.com>
2021-01-14 00:17:42 +0800

    1

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -10,10 +10,9 @@
           <div class="sidebar-sticky">
             <b-nav vertical class="pt-4">
               <b-nav-item to="/admin/user">用户管理</b-nav-item>
-              <b-nav-item to="/admin/addatasbank">常见问题库</b-nav-item>
-              <b-nav-item to="/admin/adsupplier">供应商管理</b-nav-item>
-              <b-nav-item to="/admin/adnewpro">申请提交修改</b-nav-item>
-
+              <b-nav-item to="/admin/addatasbank">商品管理</b-nav-item>
+              <b-nav-item to="/admin/adsupplier">评论管理</b-nav-item>
+              <b-nav-item to="/admin/adnewpro">学校申请</b-nav-item>
               <b-nav-item to="/admin/adanli">案例修改</b-nav-item>
               <b-nav-item to="/admin/adrankings">巡检修改</b-nav-item>
             </b-nav>
@@ -27,10 +26,31 @@
 
 <script>
 export default {
-  layout: "admin",
+  // layout: "admin",
 };
 </script>
 
 <style lang="less" scoped>
-
+.container-fluid {
+  height: 900px;
+  background-color: rgb(114, 40, 40);
+}
+.row {
+  height: 900px;
+  background-color: rgb(90, 69, 149);
+}
+.nav-link {
+  text-align: center;
+  background-color: rgb(74, 173, 79);
+}
+.nav-link:hover {
+  background-color: rgb(139, 44, 44);
+  color: #fff;
+}
+.sidebar-sticky {
+  background-color: rgb(60, 34, 134);
+}
+.bg-light {
+  padding: 0;
+}
 </style>
